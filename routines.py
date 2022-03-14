e3b20c041d5d6b3f045d965154508d4f76e37482
Spencer Bertsch <spencerbertsch1@gmail.com>
2022-03-14 09:55:15 -0400

    update formatting

--- routines.py
+++ routines.py
@@ -1,18 +1,19 @@
 """
+-------------------------------
+| Data Analytics Project Lab  |
+| Dartmouth College           |  
+| Spring 2022                 |
+-------------------------------
+
 This file contains a few helper functions needed to load the .json files from spotify to memory using 
 pandas, clean and join a few dataframes, grab the genres usingt the Spotify API. We then write the 
 resulting dataframe back down to disk as a .csv file. 
 
 This light processing is needed so that we can use the resulting .csv file as the data source for our 
 Streamlit Spotify dashboard. 
-
--------------------------------
-| Author: Spencer Bertsch     |
-| Data Analytics Project Lab  |
-| Dartmouth College           |  
-| Spring 2022                 |
--------------------------------
 """
+
+import pandas as pd
 
 
 
--- streamlit_main.py
+++ streamlit_main.py
@@ -1,3 +1,18 @@
+"""
+-------------------------------
+| Data Analytics Project Lab  |
+| Dartmouth College           |  
+| Spring 2022                 |
+-------------------------------
+
+This file contains the code needed to build a simple streamlit dashboard to display insights about a Spotify 
+user's listening behavior. 
+
+The dashboard will contain information such as the genres that are listened to the most, the hourly and daily 
+listening behavior, the top artists, and the top tracks. 
+
+This file can be run from the command line by running: $ streamlit run streamlit_main.py
+"""
 
 import pandas as pd
 import streamlit as st
