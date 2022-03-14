0247e51de3cb8d503c7c656a4ab37e843ebd8c62
Spencer Bertsch <spencerbertsch1@gmail.com>
2022-03-14 12:43:11 -0400

    add read_streaming_history function

--- routines.py
+++ routines.py
@@ -14,8 +14,34 @@ Streamlit Spotify dashboard.
 """
 
 import pandas as pd
+from pathlib import Path
+
+# define path to data (pathlib works on any operating system)
+PATH_TO_THIS_FILE: Path = Path(__file__).resolve()
+ABSPATH_TO_DATA: Path = PATH_TO_THIS_FILE.parent / "MyData"
+
+def read_streaming_history() -> pd.DataFrame:
+    """
+    Function to read the three json files as pandas dataframes and concatenate them
+    :return: pandas dataframe containing the entire streaming history for the Spotify user 
+    """
+
+    dfs = []
+    # iterate over each StreamingHistory file in the directory and read them into a pandas dataframe
+    for file_number in [0, 1, 2]:
+        fname: str = f'StreamingHistory{file_number}.json'
+        abspath_to_file: Path = ABSPATH_TO_DATA / fname
+        df: pd.DataFrame = pd.read_json(str(abspath_to_file))
+        # add the single dataframe to the list outside the loop so we can concatenate it later 
+        dfs.append(df)
+
+    # vertically concatenate all the dataframes and return the result 
+    streaming_data: pd.DataFrame = pd.concat(dfs, axis=0)
+
+    return streaming_data
 
 
+if __name__ == "__main__":
+    df = read_streaming_history()
 
-
-
+    print('something')
