48f41801e014ae16313f60020fb0e450d355fccb
Spencer Bertsch <spencerbertsch1@gmail.com>
2022-03-14 17:17:02 -0400

    add some notes to streamlit main

--- streamlit_main.py
+++ streamlit_main.py
@@ -24,3 +24,12 @@ df = pd.DataFrame({
 
 df
 
+# Create "How Well Do I Like My Own Taste in Music? The Dashboard!"
+
+# Finds the songs that you thought you would like, but you actualy hate! 
+
+# 1. Find the intersection of the songs you liked and your streaming songs 
+# 2. Group by the artist_and_song field and take the mean of the msPlayed field 
+# 3. Sort descending by the msPlayed field and display the "top" 25 songs with the least playtime 
+
+
