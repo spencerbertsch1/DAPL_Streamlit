ae7991d6292bab4c0959a1208161d347d08583d8
Spencer Bertsch <spencerbertsch1@gmail.com>
2022-03-20 21:30:23 -0400

    add plotting code

--- streamlit_main.py
+++ streamlit_main.py
@@ -14,15 +14,107 @@ listening behavior, the top artists, and the top tracks.
 This file can be run from the command line by running: $ streamlit run streamlit_main.py
 """
 
+import altair as alt
 import pandas as pd
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+from routines import SpotData
 
-df
+# read the data into memory 
+sd = SpotData()
+streaming_data = sd.streaming_history
+library = sd.library
+
+# --- get top songs ---
+top_songs_df = streaming_data.groupby(by=["artist_and_song"]).count().sort_values(by=['trackName'], ascending=False)
+top_songs_df['artist_and_song'] = top_songs_df.index
+top_songs_df = top_songs_df.reset_index(drop=True)
+top_songs_df = top_songs_df[['artist_and_song', 'endTime']].head(10)
+top_songs_df = top_songs_df.rename(columns={'endTime': 'Count'})
+
+# --- get top artists ---
+top_artist_df = streaming_data.groupby(by=["artistName"]).count().sort_values(by=['trackName'], ascending=False)
+top_artist_df['artist'] = top_artist_df.index
+top_artist_df = top_artist_df.reset_index(drop=True)
+top_artist_df = top_artist_df[['artist', 'endTime']].head(10)
+top_artist_df = top_artist_df.rename(columns={'endTime': 'Count'})
+
+# --- group for daily listening chart ---
+l_df = streaming_data.copy()
+l_df.index = pd.to_datetime(l_df['endTime'], format='%Y-%m-%d %H:%M')  
+agg_15m = l_df.groupby(pd.Grouper(freq='15Min')).count()
+
+agg_15m['d_time'] = agg_15m.index
+agg_15m['time'] = agg_15m['d_time'].dt.time
+agg_15m = agg_15m[['msPlayed', 'time']]
+
+time_df: pd.DataFrame = agg_15m.groupby(['time']).sum()
+time_df['time'] = time_df.index
+
+# ----- PLOTLY -----
+
+# fig_bar_songs = px.bar(top_songs_df, x='artist_and_song', y='Count', width=800, height=650, 
+#              color='Count', text_auto=True, title="Favorite Songs over the Past Year")
+
+# fig_bar_artists = px.bar(top_artist_df, x='artist', y='Count', width=800, height=650, 
+#              color='Count', text_auto=True, title="Favorite Artists over the Past Year")
+
+fig_bar_songs = px.bar(top_songs_df, x='Count', y='artist_and_song', height=550, # width=800, height=650, 
+             color='Count', text_auto=True, title="Favorite Songs over the Past Year", orientation='h')
+fig_bar_songs['layout']['yaxis']['autorange'] = "reversed"
+
+fig_bar_artists = px.bar(top_artist_df, x='Count', y='artist', height=550,  # width=800, height=650, 
+             color='Count', text_auto=True, title="Favorite Artists over the Past Year", orientation='h')
+fig_bar_artists['layout']['yaxis']['autorange'] = "reversed"
+
+fig_line = px.bar(time_df, x="time", y="msPlayed", width=1450, height=650, 
+                  title='Daily Listening Pattern', color='msPlayed', color_continuous_scale=px.colors.sequential.Viridis)
+
+body1 = '''
+### Part I: Genreal Music Taste!
+A look at top artists, songs, and an overview of your previous year's listening habits: 
+
+'''
+
+body2 = '''
+### Part II: How Well Do I Like My Own Taste in Music? 
+Find all the songs that you thought you would like, but you actualy hate! 
+
+'''
+
+# --- STREAMLIT CODE ---
+
+st.set_page_config(layout="wide")
+
+st.header('Spotify Dashboard', anchor=None)
+
+st.markdown('Author: [Spencer Bertsch](https://github.com/spencerbertsch1). Thanks to my pal [Mike Koshakow](https://github.com/Cpt-Catnip) for letting me show his music preferences to the world!', unsafe_allow_html=False)
+
+st.markdown(body1, unsafe_allow_html=False)
+
+
+
+col1, col2= st.columns(2)
+
+with col1:
+    st.header("Top Songs")
+    st.plotly_chart(fig_bar_songs, use_container_width=True)
+
+with col2:
+    st.header("Top Artists")
+    st.plotly_chart(fig_bar_artists, use_container_width=True)
+
+st.markdown('We can now look at the listening pattern through out the day! Do you like to \
+listen to music in the morning? In the evening? Perhaps a podcast over lunch? Let\'s find out.', unsafe_allow_html=False)
+
+st.plotly_chart(fig_line, use_container_width=False)
+
+st.markdown(body2, unsafe_allow_html=False)
+
+
 
 # Create "How Well Do I Like My Own Taste in Music? The Dashboard!"
 
